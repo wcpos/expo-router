ba8ce81f4eaf3680ffe066c318a067f0122828b3
Paul Kilmurray <paul@kilbot.com>
2024-07-01 16:00:58 +0100

    Initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import '../global.css';
+import { Slot } from 'expo-router';
+
+import { SessionProvider } from '../ctx';
+
+export default function Root() {
+	// Set up the auth context and render our layout inside of it.
+	return (
+		<SessionProvider>
+			<Slot />
+		</SessionProvider>
+	);
+}
